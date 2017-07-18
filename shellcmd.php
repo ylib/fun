8218c6e62d7e6430741f9ad42879da3a916cc446
ylib <ylib@outlook.com>
2017-07-18 10:57:21 -0500

    Additional paranoia

--- shellcmd.php
+++ shellcmd.php
@@ -1,36 +1,51 @@
 <?php
-    function permitted($req) {
-        $safe = array("ceph status","ceph osd tree","ls -l");
-        return in_array($req, $safe);
+    function sanitize($data)
+    {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
     }
 
-    $cmd = $_GET['c']; //shell command string
-    $sec = $_GET['r']; //number of seconds
+    function permitted($req)
+    {
+        $safe = array("ps -e","ls -l","ceph status","ceph osd tree");
+        if ($req) {
+            return in_array($req, $safe);
+        }
+        else
+        {
+            return $safe;
+        }
+    }
 
-    $body = '<p>Use ShellCmd to view the output of any authorized command.</p>';
+    $cmd = escapeshellcmd($_GET['c']); //shell command string
+    $sec = sanitize($_GET['r']); //number of seconds
 
-    if (is_numeric($sec)) {
+    $body = '<p>Use ShellCmd to view the output of any of these authorized commands.</p>';
+    $body .= '<ul><li>' . implode(permitted(),'</li><li>') . '</li></ul>';
+
+    if (is_integer($sec)) {
         $ms = $sec * 1000;
     }
 
     if ($cmd) {
         if (permitted($cmd)) {
             if ($ms) {
-                $refresh = "<script>setTimeout(function(){location.reload(true)},$ms);</script>";
+                $refresh = "<script>setTimeout(function(){location.reload(true)},$ms);";
             }
             $body = "<p>Output of $cmd...</p><pre>" . shell_exec($cmd) . "</pre>" . $refresh;
         } else {
             $body = "<h1>Illegal Request!</h1><p>You cannot use shellcmd.php to execute $cmd</p>";
         }
     }
-?>
-<!DOCTYPE html>
+?><!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>ShellCmd by yLib</title>
+    <title>ShellCmd (by Chris Kramer)</title>
     <style>
     html, body {
         height: 100%;
